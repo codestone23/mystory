9c35f4b3b8a1766089284ac6e7f0711f19f1f889
Phạm Gia Đạt <84780233+codestone23@users.noreply.github.com>
2023-03-25 16:14:56 +0700

    create button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,6 @@
 
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-// background
-let container = $(".sub-item #container")
-
-let images = $$("#container .imgs")
-
-for (let i of images) {
-  moveImage(i);
-}
-function clickImgs(){
-  images.forEach((item,index) =>{
-    item.addEventListener('mousemove', function() {
-      item.style.transform = 'scale(1.2)'; 
-    });
-    item.addEventListener('mouseleave', function() {
-      item.style.transform = 'scale(1)'; 
-    });
-  })
-}
-clickImgs();
-
-function moveImage(img) {
-    let maxX = container.offsetWidth - img.width;
-    let maxY = container.offsetHeight - img.height;
-    let posX = Math.floor(Math.random() * maxX);
-    let posY = Math.floor(Math.random() * maxY);
-    let dirX = Math.random() < 0.5 ? -1 : 1;
-    let dirY = Math.random() < 0.5 ? -1 : 1;
-    let speedX = Math.floor(Math.random() * 1.5) + 1;
-    let speedY = Math.floor(Math.random() * 1.5) + 1;
-  setInterval(function() {
-    posX += speedX * dirX;
-    posY += speedY * dirY;
-    if (posX < 0 || posX > maxX) {
-      dirX = -dirX;
-    }
-    if (posY < 0 || posY > maxY) {
-      dirY = -dirY;
-    }
-    img.style.left = posX + "px";
-    img.style.top = posY + "px";
-  }, 20);
-}
 
 // my story
 const MY_STORY = {
@@ -62,11 +20,80 @@ const MY_STORY = {
     nextSlide: $(".slider #next-slide"),
     // skill
     skillElement: $(".skill"),
+    // container img
+    container: $(".sub-item #container"),
+    images: $$("#container .imgs"),
+    // more hide
+    afterItem:$$(".sub-item .after-item"),
+    buttonDown: $(".sub-item .button-down"),
+    buttonUp: $(".sub-item .button-up"),
+
+
 
     start: function(){
       this.handleClickNav();
       this.handleScroll();
       this.handleSlider();
+      this.handleClickImg();
+      for (let i of this.images) {
+          this.handleMoveImage(i);
+      };
+      this.handleMore();
+      this.handleHide();
+    },
+    // moveImage
+    handleMoveImage: function(img) {
+        let maxX = this.container.offsetWidth - img.width;
+        let maxY = this.container.offsetHeight - img.height;
+        let posX = Math.floor(Math.random() * maxX);
+        let posY = Math.floor(Math.random() * maxY);
+        let dirX = Math.random() < 0.5 ? -1 : 1;
+        let dirY = Math.random() < 0.5 ? -1 : 1;
+        let speedX = Math.floor(Math.random() * 1.5) + 1;
+        let speedY = Math.floor(Math.random() * 1.5) + 1;
+      setInterval(function() {
+        posX += speedX * dirX;
+        posY += speedY * dirY;
+        if (posX < 0 || posX > maxX) {
+          dirX = -dirX;
+        }
+        if (posY < 0 || posY > maxY) {
+          dirY = -dirY;
+        }
+        img.style.left = posX + "px";
+        img.style.top = posY + "px";
+        }, 20);  
+    },
+    // mousemove
+    handleClickImg: function(){
+      this.images.forEach((item,index) =>{
+        item.addEventListener('mousemove', function() {
+          item.style.transform = 'scale(1.2)'; 
+        });
+        item.addEventListener('mouseleave', function() {
+          item.style.transform = 'scale(1)'; 
+        });
+      })
+    },
+    // more item
+    handleMore : function(){
+      this.buttonDown.onclick = () =>{
+          this.afterItem.forEach((item,index)=>{
+              item.classList.remove("hide-item")
+          })
+          this.buttonDown.setAttribute("style","display:none")
+          this.buttonUp.removeAttribute("style")
+      }
+  },
+    // hide item
+    handleHide : function(){
+        this.buttonUp.onclick = () =>{
+          this.afterItem.forEach((item,index)=>{
+            item.classList.add("hide-item")
+        })
+        this.buttonUp.setAttribute("style","display:none")
+        this.buttonDown.removeAttribute("style")
+        }
     },
     // function handleClickNav
     handleClickNav: function(){
